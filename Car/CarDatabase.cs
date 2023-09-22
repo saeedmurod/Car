79c53f600fdaa1cc43e83e49eb0aa05eb7796ad4
Saidmurodkhodja <saidmurod11@gmail.com>
2023-09-22 15:23:38 +0500

    Добавьте файлы проекта.

---
+++ Car/CarDatabase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class CarDatabase
+    {
+        private static CarDatabase instance;
+        private readonly Dictionary<string, List<Car>> data;
+
+        private CarDatabase()
+        {
+            data = new Dictionary<string, List<Car>>();
+        }
+
+        public static CarDatabase Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CarDatabase();
+                }
+                return instance;
+            }
+        }
+
+        public void AddCar(string brand, string model, int quantity, decimal cost)
+        {
+            if (!data.ContainsKey(brand))
+            {
+                data[brand] = new List<Car>();
+            }
+
+            data[brand].Add(new Car { Model = model, Quantity = quantity, Cost = cost });
+        }
+
+        public int CountTypes()
+        {
+            return data.Count;
+        }
+
+        public int CountAll()
+        {
+            return data.Values.Sum(brandData => brandData.Sum(car => car.Quantity));
+        }
+
+        public decimal AveragePrice()
+        {
+            int totalQuantity = data.Values.SelectMany(brandData => brandData).Sum(car => car.Quantity);
+            decimal totalPrice = data.Values.SelectMany(brandData => brandData).Sum(car => car.Quantity * car.Cost);
+
+            if (totalQuantity == 0)
+            {
+                return 0;
+            }
+
+            return totalPrice / totalQuantity;
+        }
+
+        public decimal AveragePriceByBrand(string brand)
+        {
+            if (data.ContainsKey(brand))
+            {
+                var brandData = data[brand];
+                int totalQuantity = brandData.Sum(car => car.Quantity);
+                decimal totalPrice = brandData.Sum(car => car.Quantity * car.Cost);
+
+                if (totalQuantity == 0)
+                {
+                    return 0;
+                }
+
+                return totalPrice / totalQuantity;
+            }
+
+            return 0;
+        }
+
+        public IEnumerable<string> GetBrands()
+        {
+            return data.Keys;
+        }
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,33 @@
+﻿using Car;
+
+CarDatabase honda = CarDatabase.Instance;
+honda.AddCar("Honda", "Civic", 1, 8000);
+while (true)
+{
+    Console.WriteLine("Enter a command (count types, count all, average price, average price type, exit):");
+    string command = Console.ReadLine().ToLower();
+
+    switch (command)
+    {
+        case "count types":
+            Console.WriteLine($"Number of car brands: {CarDatabase.Instance.CountTypes()}");
+            break;
+        case "count all":
+            Console.WriteLine($"Total number of cars: {CarDatabase.Instance.CountAll()}");
+            break;
+        case "average price":
+            Console.WriteLine($"Average cost of a car: {CarDatabase.Instance.AveragePrice():C}");
+            break;
+        case "average price type":
+            Console.WriteLine("Enter the car brand:");
+            string brand = Console.ReadLine();
+            decimal averagePrice = CarDatabase.Instance.AveragePriceByBrand(brand);
+            Console.WriteLine($"Average cost of {brand} cars: {averagePrice:C}");
+            break;
+        case "exit":
+            return;
+        default:
+            Console.WriteLine("Invalid command.");
+            break;
+    }
+}
